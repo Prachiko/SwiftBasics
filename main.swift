6b7d2c575a2c1c5396113a43719204961b0ea3c7
Prachiko <Prachikolekar20@gmail.com>
2024-08-18 17:00:38 +0530

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  myswiftbasics
+//  Lecturebasics
 //
 //  Created by prachinarvekar on 18/08/24.
 //
@@ -9,91 +9,71 @@ import Foundation
 
 print("Hello, World!")
 
-// Online Swift compiler to run Swift program online
-// simple values//
-var str = "Hello, playground"
-var str1 = "hello, playground\n"
-print(str)
-print(str1)
+let str = "Hello world!"
+var _str = "Hello world!"
+_str = "Hey"
+print(str, _str)
 
-// implicit & explicit//
-let implicitInteger = 70
-let implicitDouble = 70.0
-let explicitDouble: Double = 70
-print(explicitDouble)
+let myint = 0
+var _myint = 2
+_myint += 1
+print(myint, _myint)
 
- //adding two constant ////
-let label = "the width is"
-let width = 9
-let widthLable = label +   String(width)
-print(label)
-print(width)
-print(widthLable)
+print(_myint/2)
 
-//// use of backslash for adding values ////
-let apples = 3
-let oranges = 5
-let appleSummary = "I have \(apples) apples."
-let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+let myDouble = Double(_myint)
+print(myDouble/2)
 
-let iphone = 2
-let mac = 6
-let macsummary = "I have \(mac) mac"
-let DevicesSummary = " I have \(iphone + mac) devices. "
+let myDecimalstr = "13.37"
+let myCastedDouble = Double(myDecimalstr)
 
-var myVariable = 42
-myVariable = 50
-let myConstant = 42
+//let myvar: String = "\(myCastedDouble)"
 
-///Arrays///
+//let double: Double = 1.0
+//let optionalDouble: Double? = nil
 
-var fruits = ["strawberries", "limes", "tangerines"]
-fruits[1] = "grapes"
+//let sum: Double? = double + (optionalDouble ?? )
 
-var occupations = [
-    "Malcolm": "Captain",
-    "Kaylee": "Mechanic",
- ]
-occupations["Jayne"] = "Public Relations"
-fruits.append("blueberries")
-print(fruits)
+var arr: [Any] = []
+if arr.indices.contains(0) {
+    print("found something")
+}
+else
+{
+    print("boo")
+}
+arr.append(1)
+arr.append(2)
+arr.append (3)
+arr.append(4)
+arr.append("50")
+arr.append("50.54")
+print(arr) // [1, 2, 3, 4, "50", "50.54"]
 
-/// for in loop in array//
-let individualScores = [75, 43, 103, 87, 12]
-var teamScore = 0
-for score in individualScores {
-    if score > 50 {
-        teamScore += 3
+let firstObject = Double( (arr[0] as? Int) ?? 0)
+// 0
+print(firstObject) // 1.0
+let fourthObject = (arr[4] as? Double) ?? 5
+print(fourthObject) // 5.0
+let sum2 = firstObject + fourthObject  // 1.0 + 5.0
+print(arr) //  [1, 2, 3, 4, "50", "50.54"]
+let arr2 = arr.dropLast()
+print(arr2) // [1, 2, 3, 4, "50"]
+print(sum2)
+
+let moreThan3 = arr.compactMap { value in
+    if let integer = value as? Int, integer >= 3 {
+        print(integer)
+        return integer
     } else {
-        teamScore += 1
+        return nil
     }
 }
-print(teamScore)
 
-////optionalString////
-var optionalString: String? = "Hello"
-print(optionalString == nil)
-// Prints "false"
 
-//var optionalName: String? = "John Appleseed"
-//var greeting = "Hello!"
-//if let name = optionalName {
-//    greeting = "Hello, \(name)"
-//let nickname: String? = nil
-//let fullName: String = "John Appleseed"
-//let if let nickname {
- //   print("Hey, \(nickname)")
-//} = "Hi \(nickname ?? fullName)"
 
-// switches//
-let vegetable = "red pepper"
-switch vegetable {
-case "celery":
-    print("Add some raisins and make ants on a log.")
-case "cucumber", "watercress":
-    print("That would make a good tea sandwich.")
-case let x where x.hasSuffix("pepper"):
-    print("Is it a spicy \(x)?")
-default:
-    print("Everything tastes good in soup.")
-}
+
+
+
+
+
